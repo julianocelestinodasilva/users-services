eff2ab65598b46f544da7988e7599feb0c1fe100
juliano <jcelestino@raiadrogasil.com.br>
2018-08-06 20:36:28 -0300

    Criando End Point do User (UserResource)

---
+++ src/main/java/julianocelestino/usersservices/UserResource.java
@@ -0,0 +1,24 @@
+package julianocelestino.usersservices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+
+@Controller
+@RequestMapping(path="/users")
+public class UserResource {
+
+	public void a() {
+	}
+
+
+}
